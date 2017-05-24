d0e23c1db723d44f99167d220b84cfe745e706c5
Vasile2kappa <andy.dorcu@yahoo.com>
2017-05-24 21:46:27 +0300

    Some testing

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import ctypes
+from time import sleep
 
 KEYEVENTF_EXTENDEDKEY = 0x0001
 KEYEVENTF_KEYUP       = 0x0002
@@ -20,6 +21,18 @@ def playGame(game):
 	return
 
 def main():
+
+	while True:
+		pressKey(VK_NUMLOCK)
+		sleep(0.2)
+		pressKey(VK_NUMLOCK)
+		pressKey(VK_CAPITAL)
+		sleep(0.2)
+		pressKey(VK_CAPITAL)
+		pressKey(VK_SCROLL)
+		sleep(0.2)
+		pressKey(VK_SCROLL)
+
 	return
 
 if __name__ == '__main__':
